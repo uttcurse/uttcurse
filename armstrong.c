766be27c083c3dc53a4fbfca77f802e693d71608
Utkarsh Brahma <48096691+uttcurse@users.noreply.github.com>
2022-09-20 10:53:44 +0530

    Add files via upload

---
+++ armstrong.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    printf("Enter a number : ");
+    int n,i;
+    scanf("%d",&n);
+    int m=n,d,s=0;
+    while (n>0)
+    {
+        d=n%10;
+        s=s+d*d*d;
+        n=n/10;
+    }
+    if (s==m)
+    {
+        printf("%d is a armstrong number",m);
+    }
+    else 
+    {
+        printf("%d is a not armstrong number",m);
+    }
+}
